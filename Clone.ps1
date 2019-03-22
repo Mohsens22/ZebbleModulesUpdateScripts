daa3aaa60c75314ea481e38e2f0ce40cc5903202
Mohsen Seifi <mohsens2@outlook.com>
2019-03-22 18:25:03 +0430

    Update Clone.ps1

--- Clone.ps1
+++ Clone.ps1
@@ -1,4 +1,5 @@
 foreach($line in Get-Content .\gitControl.txt) {
     $toClone = "git clone https://github.com/Geeksltd/"+$line+"/";
-    echo $toClone
+    echo $toClone;
+    Invoke-Expression $toClone;
 }
